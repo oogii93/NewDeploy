03f8bef725286d76f1f46eae14376068fc64aac5
nymaa <ba20e065@student.humanities.mn>
2024-12-24 18:04:37 +0900

    adfsafds

--- resources/views/ac/ac_table.blade.php
+++ resources/views/ac/ac_table.blade.php
@@ -22,7 +22,7 @@
             <tr class="border-b border-gray-200 hover:bg-gray-200">
                 <td class="border border-slate-300 px-4 py-2">{{ $application->id }}</td>
 
-                <td class="border border-slate-300 px-4 py-2">{{ $application->user->name }}</td>
+                <td class="border border-slate-300 px-4 py-2">{{ $application->user->name ?? 'N/A' }}</td>
 
 
                 <td class="border border-slate-300 px-4 py-2 whitespace-nowrap hidden md:table-cell">{{ $application->applicationable_id }}</td>
